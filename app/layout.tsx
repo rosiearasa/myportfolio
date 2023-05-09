e3e9fac568a7facace397a88108a8b18dab65af6
rosiearasa <rosearasa7522@gmail.com>
2023-05-09 18:47:13 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,66 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+	title: {
+		default: "rosiearasa.com",
+		template: "%s | rosiearasa.com",
+	},
+	description: "Software engineer",
+	openGraph: {
+		title: "rosiearasa.com",
+		description:
+			"Software engineer",
+		url: "https://rosiearasa.com",
+		siteName: "rosiearasa.com",
+	
+		locale: "en-US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	
+	icons: {
+		shortcut: "/favicon.png",
+	},
+};
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				<Analytics />
+				{children}
+			</body>
+		</html>
+	);
+}
